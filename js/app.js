742a492fcc16ac5dc6ed8b6124402e7b13501f9d
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-07-23 11:25:00 +0100

    Fixed Linters

--- js/app.js
+++ js/app.js
@@ -3,21 +3,23 @@ const blurList = ['#logo', '#headline', 'main'];
 const navHeader = document.querySelector('.nav-header');
 
 function handleNavigation(e) {
-    const { target } = e;
+  const { target } = e;
+  if (target.matches('#logo, .hamburger-menu, .Portforlio *, .About *, .Contact *')) {
     e.preventDefault();
-    if (target.matches('.hamburger-menu, .Portforlio *, .About *, .Contact *')) {
-        checkbox.checked = !checkbox.checked;
-        blurList.forEach(item => document.querySelector(item).classList.toggle('blur'));
-        document.documentElement.classList.toggle('disable-overflow');
-    } 
-    
-    if (target.matches('.Portforlio *')) {
-        document.querySelector('#works').scrollIntoView({ behavior: 'smooth' });
-    } else if (target.matches('.About *')) {
-        document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
-    } else if (target.matches('.Contact *')) {
-        document.querySelector('#contact-form').scrollIntoView({ behavior: 'smooth' });
-    }
+    checkbox.checked = !checkbox.checked;
+    blurList.forEach((item) => document.querySelector(item).classList.toggle('blur'));
+    document.documentElement.classList.toggle('disable-overflow');
+  }
+
+  if (target.matches('.Portforlio *')) {
+    document.querySelector('#works').scrollIntoView({ behavior: 'smooth' });
+  } else if (target.matches('.About *')) {
+    document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
+  } else if (target.matches('.Contact *')) {
+    document.querySelector('#contact-form').scrollIntoView({ behavior: 'smooth' });
+  } else {
+    document.querySelector('header').scrollIntoView({ behavior: 'smooth' });
+  }
 }
 
 navHeader.addEventListener('click', handleNavigation);

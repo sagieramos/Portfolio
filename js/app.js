36406e28283bef52777f9b22c7c80783a9773534
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-07-23 17:53:16 +0100

    code optimised

--- js/app.js
+++ js/app.js
@@ -2,24 +2,26 @@ const checkbox = document.querySelector('#menu-checkbox');
 const blurList = ['#logo', '#headline', 'main'];
 const navHeader = document.querySelector('.nav-header');
 
+const sections = {
+  portfolio: document.querySelector('#works'),
+  about: document.querySelector('#about'),
+  contact: document.querySelector('#contact-form'),
+  header: document.querySelector('header'),
+};
+
 function handleNavigation(e) {
   const { target } = e;
+  let sectionId = 'header';
   if (target.matches('#logo, .hamburger-menu, .Portforlio *, .About *, .Contact *')) {
     e.preventDefault();
     checkbox.checked = !checkbox.checked;
     blurList.forEach((item) => document.querySelector(item).classList.toggle('blur'));
     document.documentElement.classList.toggle('disable-overflow');
   }
-
-  if (target.matches('.Portforlio *')) {
-    document.querySelector('#works').scrollIntoView({ behavior: 'smooth' });
-  } else if (target.matches('.About *')) {
-    document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
-  } else if (target.matches('.Contact *')) {
-    document.querySelector('#contact-form').scrollIntoView({ behavior: 'smooth' });
-  } else {
-    document.querySelector('header').scrollIntoView({ behavior: 'smooth' });
-  }
+  if (target.matches('.Portforlio *')) sectionId = 'portfolio';
+  else if (target.matches('.About *')) sectionId = 'about';
+  else if (target.matches('.Contact *')) sectionId = 'contact';
+  sections[sectionId].scrollIntoView({ behavior: 'smooth' });
 }
 
 navHeader.addEventListener('click', handleNavigation);

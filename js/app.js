f5be5fa2212b67617cdbf42a646da865981980c3
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-07-24 09:00:27 +0100

    bug fixed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const checkbox = document.querySelector('#menu-checkbox');
 const blurList = ['#logo', '#headline', 'main'];
 const navHeader = document.querySelector('.nav-header');
+let nav = false;
 
 const sections = {
   portfolio: document.querySelector('#works'),
@@ -12,17 +13,21 @@ const sections = {
 function handleNavigation(e) {
   const { target } = e;
   let sectionId = 'header';
-  e.preventDefault();
   if (target.matches('.hamburger-menu, .Portforlio *, .About *, .Contact *')) {
     checkbox.checked = !checkbox.checked;
     blurList.forEach((item) => document.querySelector(item).classList.toggle('blur'));
     document.documentElement.classList.toggle('disable-overflow');
+    e.preventDefault();
+    nav = true;
   }
   if (target.matches('.Portforlio *')) sectionId = 'portfolio';
   else if (target.matches('.About *')) sectionId = 'about';
   else if (target.matches('.Contact *')) sectionId = 'contact';
-  else sectionId = 'header';
-  sections[sectionId].scrollIntoView({ behavior: 'smooth' });
+  else if (target.matches('#logo')) sectionId = 'header';
+  if (nav) {
+    sections[sectionId].scrollIntoView({ behavior: 'smooth' });
+    nav = false;
+  }
 }
 
 navHeader.addEventListener('click', handleNavigation);
